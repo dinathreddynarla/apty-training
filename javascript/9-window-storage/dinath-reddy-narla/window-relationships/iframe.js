03895efa50655cab7786e9879a5ab93228a94af6
dinathreddynarla <reddydinath18@gmail.com>
2025-03-24 18:57:21 +0530

    assignment 9 window objects by dinath reddy narla-3

--- javascript/9-window-storage/dinath-reddy-narla/window-relationships/iframe.js
+++ javascript/9-window-storage/dinath-reddy-narla/window-relationships/iframe.js
@@ -1,12 +1,13 @@
 const sendMessageButton = document.getElementById('sendMessageToParent');
 const iframeMessage = document.getElementById('iframeMessage');
+console.log(window.top == window);
 
 // Send a message to the parent window when the button is clicked
 sendMessageButton.addEventListener('click', () => {
-    window.parent.postMessage('Hello from iframe!', '*');
+    window.parent.postMessage({type: "iframe-to-parent" ,message :"Hello from Iframe!"}, '*');
 });
 
 // Listen for messages from the parent window
 window.addEventListener('message', (event) => {
-        iframeMessage.textContent = `Message received in iframe: ${event.data}`;
+        iframeMessage.textContent = `Message received in iframe: ${event.data.message}`;
 });
--- javascript/9-window-storage/dinath-reddy-narla/window-relationships/script.js
+++ javascript/9-window-storage/dinath-reddy-narla/window-relationships/script.js
@@ -4,17 +4,24 @@ const parentMessage = document.getElementById('parentMessage');
 
 // Send a message to the iframe (child window) when the button is clicked
 sendMessageButton.addEventListener('click', () => {
-    iframe.contentWindow.postMessage('Hello from parent!', '*');
+    iframe.contentWindow.postMessage({type: "parent-to-iframe" ,message :"Hello from parent!"}, '*');
 });
 
 // Listen for messages from the iframe (child window)
 window.addEventListener('message', (event) => {
-    if (event.source === window) {
-        return; // Ignore messages sent by this window (parent)
-    }
-    // Ensure the message is from the iframe
-    if (event.origin === window.location.origin && event.source === iframe.contentWindow) {
-        parentMessage.textContent = `Message received in parent: ${event.data}`;
+    // if (event.source === window) {
+    //     return; // Ignore messages sent by this window (parent)
+    // }
+    // // Ensure the message is from the iframe
+    // if (event.origin === window.location.origin && event.source === iframe.contentWindow) {
+    //     console.log(event);
+        
+    //     parentMessage.textContent = `Message received in parent: ${event.data}`;
+    // }
+    
+    if(event.data.type == "iframe-to-parent"){
+        console.log(event.data);  
+        parentMessage.textContent = `Message received in parent: ${event.data.message}`;
     }
 
 });
